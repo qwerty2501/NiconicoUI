521006a63ebce3cb5bef7f6ae36a9c7cb04e01bb
クワーティ <riot313@gmail.com>
2014-01-29 21:41:07 +0900

    changed namespace

---
+++ NiconicoUI/Onds.Niconico.UI.WinRT/Extentions/NiconicoTextColorUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Onds.Niconico.Data.Text;
+
+
+
+namespace Onds.Niconico.UI.Extentions
+{
+
+#if NETFX_CORE
+    using Windows.UI;
+#endif
+
+
+    public static class NiconicoTextColorUtility
+    {
+
+        public Color ToPlatFormColor(this NiconicoTextColor color)
+        {
+            return Color.FromArgb(0xFF, color.R, color.G, color.B);
+        }
+
+        public NiconicoTextColor ToNiconicoTextColor(this Color color)
+        {
+            return new NiconicoTextColor { R = color.R, G = color.G, B = color.B };
+        }
+    }
+}
