5acc001e7b4c84043e8958565e92b7ddfb7a59ef
クワーティ <riot313@gmail.com>
2014-01-30 23:09:27 +0900

    implemented INiconicoWebTextSegmentExtention

--- NiconicoUI/Onds.Niconico.UI.WinRT/Extentions/INiconicoWebTextSegmentExtention.cs
+++ NiconicoUI/Onds.Niconico.UI.WinRT/Extentions/INiconicoWebTextSegmentExtention.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿#if NETFX_CORE
+using Windows.UI;
+#endif
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Onds.Niconico.Data.Text;
+
 
 namespace Onds.Niconico.UI.Extentions
 {
-    class INiconicoWebTextSegmentExtention
+    public static class INiconicoWebTextSegmentExtention
     {
+        public static Color GetPlatformColor(this IReadOnlyNiconicoWebTextSegment self)
+        {
+            return self.Color.ToPlatFormColor();
+        }
+
+        public static double CorrectToFontElementSize(this IReadOnlyNiconicoWebTextSegment self,double fontSize)
+        {
+            return (fontSize * self.FontElementSize) / 3;
+        }
     }
 }
